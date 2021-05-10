ad90c66730b093844e80bcb7d659d51b93804d0e
Kenny Chen <kennychen851228@gmail.com>
2021-05-10 15:06:49 +0800

    Feat: Refactor

--- pkg/service/base.go
+++ pkg/service/base.go
@@ -1,36 +1,34 @@
 package service
 
 import (
+	"net/http"
+
+	"github.com/KennyChenFight/gin-starter/pkg/business"
 	"github.com/KennyChenFight/gin-starter/pkg/dao"
-	"github.com/KennyChenFight/gin-starter/pkg/util"
 	"github.com/gin-gonic/gin"
 )
 
 type BaseService struct {
-	memberDAO            dao.MemberDAO
-	validationTranslator *util.ValidationTranslator
+	memberDAO dao.MemberDAO
 }
 
-func NewService(memberDAO dao.MemberDAO, validatorTranslator *util.ValidationTranslator) *BaseService {
-	return &BaseService{memberDAO: memberDAO, validationTranslator: validatorTranslator}
+func NewService(memberDAO dao.MemberDAO) *BaseService {
+	return &BaseService{memberDAO: memberDAO}
 }
 
-func sendErrorResponse(c *gin.Context, service *BaseService, businessError *util.BusinessError) {
-	translated, err := service.validationTranslator.Translate(c.GetHeader("Accept-Language"), businessError.Reason)
-	if translated == nil && err == nil {
-		c.JSON(businessError.HTTPStatusCode, businessError)
-		return
-	}
-
-	// translate fail, you can logging.
-	if err != nil {
-		c.JSON(businessError.HTTPStatusCode, businessError)
-	} else {
-		businessError.ValidationErrors = translated
-		c.JSON(businessError.HTTPStatusCode, businessError)
-	}
+func (s *BaseService) HandleMethodNotAllowed(c *gin.Context) {
+	s.responseWithError(c, business.NewError(business.MethodNowAllowed, http.StatusMethodNotAllowed, "http method not allowed", nil))
 }
 
-func sendSuccessResponse(c *gin.Context, statusCode int, response interface{}) {
-	c.JSON(statusCode, response)
+func (s *BaseService) HandlePathNotFound(c *gin.Context) {
+	s.responseWithError(c, business.NewError(business.PathNotFound, http.StatusNotFound, "http path not found", nil))
+}
+
+func (s *BaseService) responseWithError(c *gin.Context, businessError *business.Error) {
+	c.Abort()
+	c.Error(businessError)
+}
+
+func (s *BaseService) responseWithSuccess(c *gin.Context, businessSuccess *business.Success) {
+	c.Set("success", businessSuccess)
 }
